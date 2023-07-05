518845040d56b5249956b954dfca78a9cc959008
samararu <129498561+samararu@users.noreply.github.com>
2023-07-06 01:35:19 +0400

    Update cfg.py

--- cfg.py
+++ cfg.py
@@ -1,3 +1,3 @@
-token='' # токен сообщества
-acces_token = '' # токен  страницы ВК
+group_access_token ='' # токен сообщества
+user_access_token = '' # токен  страницы ВК
 db_url_object ="" # строка подключения к бд
