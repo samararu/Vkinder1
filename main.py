f667f372fefd4a52427e44234550fc811edc4cda
samararu <129498561+samararu@users.noreply.github.com>
2023-07-05 15:29:58 +0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import vk_api
+import cfg
+from vk_api.longpoll import VkLongPoll, VkEventType
+
+import vk_bot
+
+import threading
+
+myBot = vk_bot.VkBot()
+longpoll = myBot.get_long_pool()
+
+for event in longpoll.listen():
+    if event.type == VkEventType.MESSAGE_NEW:
+        if event.to_me:
+            request = event.text
+            print(request)
+            threading.Thread(target=myBot.processing_message, args=(event.user_id, request)).start()
